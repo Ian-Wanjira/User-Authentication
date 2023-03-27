81c478a9f10359652b598bd48f495349c29f6a25
Ian-Wanjira <ianwanjira4@gmail.com>
2023-03-27 04:08:28 +0300

    create: RegisterView with get and post methods defined

--- authentication/views.py
+++ authentication/views.py
@@ -1,1 +1,22 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.views import View
+from .forms import CustomUserCreationForm
+
+class RegisterView(View):
+    def get(self, request):
+        form = CustomUserCreationForm()
+        return render(request, 'register.html', {'form': form})
+
+    def post(self, request):
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}!')
+            return redirect('login')
+        else:
+            return render(request, 'register.html', {'form': form})
+
 
